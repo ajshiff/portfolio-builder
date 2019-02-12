44d725226ff86193bf5f8774f226a1a44d5e286b
ajshiff <ajshiffler@gmail.com>
2019-02-12 02:19:39 -0700

    Finished code that will cycle through html snippets and add them to the projects div.

--- generateHtml.js
+++ generateHtml.js
@@ -1,21 +1,23 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-function generateHtml (projectList) {
-    // TODO make a more dynamic input for htmlTemplate file.
-    let htmlTemplate = fs.readFileSync('./template-index.html'); 
+function generateHtml (projectList, htmlTemplate = './template-index.html') {
+    htmlTemplate = fs.readFileSync(htmlTemplate); 
     let $ = cheerio.load(htmlTemplate);
     let htmlProjectSnippits = projectList.map(generateHtmlSnippits);
-    let htmlDoc = htmlProjectSnippits.reduce(addHtmlSnippits, $);
+    let htmlDoc = htmlProjectSnippits.reduce(addHtmlSnippits, $).html();
+    console.log(htmlDoc)
     return htmlDoc;
 }
  
 function generateHtmlSnippits (project) {
+    project = `\n<span>This Project is called ${project.name}</span>`;
     return project;
 }
 
 function addHtmlSnippits ($, projectSnippit) {
     let projectsDiv = $('.portfolioBuilder #projects').first();
+    projectsDiv.append(projectSnippit);
     return $;
 }
 
