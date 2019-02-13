35e03f6749a7133f8f8c33eac0d353cb7558d4af
ajshiff <ajshiffler@gmail.com>
2019-02-13 00:12:27 -0700

    added block comment blocks on functions

--- generateHtml.js
+++ generateHtml.js
@@ -1,6 +1,9 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+/***********************************************************
+ * 
+ ***********************************************************/
 function generateHtml (projectList, htmlTemplate = './template-index.html') {
     htmlTemplate = fs.readFileSync(htmlTemplate); 
     let $ = cheerio.load(htmlTemplate);
@@ -10,11 +13,17 @@ function generateHtml (projectList, htmlTemplate = './template-index.html') {
     return htmlDoc;
 }
  
+/***********************************************************
+ * 
+ ***********************************************************/
 function generateHtmlSnippits (project) {
     project = `\n<span>This Project is called ${project.name}</span>`;
     return project;
 }
 
+/***********************************************************
+ * 
+ ***********************************************************/
 function addHtmlSnippits ($, projectSnippit) {
     let projectsDiv = $('.portfolioBuilder #projects').first();
     projectsDiv.append(projectSnippit);
